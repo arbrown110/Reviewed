ac4828d9e657ff30291498cc437ad16caeaf08bc
Angel_Brown <arbrown110@gmail.com>
2021-07-22 16:11:33 -0400

    creating styleheet and about function in reviewDetail.js .

---
+++ screens/reviewDeatils.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function RevieDetails() {
+    return (
+        <View> style={StyleSheet.container}
+            <Text>Deatil Screen </Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+});
