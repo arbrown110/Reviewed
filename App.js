8a05cba398807e201ae81a6a9ab5e3eabf47797d
Angel_Brown <arbrown110@gmail.com>
2021-07-29 16:36:19 -0400

    importing font settings into app.js.

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import * as Font from 'expo-font';
 import Home from './screens/home';
 
+
+const getFonts = () => {
+  return Font.loadAsync({
+    
+  })
+}
 
 export default function App() {
   return (
