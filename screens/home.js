259c4d3498de72ccaf916c18a5c633c4e8a2a3d9
Angel_Brown <arbrown110@gmail.com>
2021-08-04 18:31:06 -0400

    addi8ng titletext to style stylesheet.

--- screens/home.js
+++ screens/home.js
@@ -14,5 +14,9 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         padding: 24
+    },
+    titletext: {
+        fontfamily: 'nunito-bold',
+        frontSize: 18,
     }
 });
