f345f902ed1b637e30ea9c2a551ce1f7e06066b4
Angel_Brown <arbrown110@gmail.com>
2021-08-04 18:38:19 -0400

    moving styles function to global.js.

---
+++ styles/global.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    },
+    titletext: {
+        fontfamily: 'nunito-bold',
+        frontSize: 18,
+    }
+});
