4fcba83467b0d8f578d5627c7e84e4b5ce7d3b45
Angel_Brown <arbrown110@gmail.com>
2021-08-09 19:08:39 -0400

    adding to globaalstyles  function in global.js.

--- styles/global.js
+++ styles/global.js
@@ -1,11 +1,14 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
+export const globelStyles = StyleSheet.create({
     container: {
         padding: 24
+        flex: 1,
+        padding: 20,
     },
     titletext: {
         fontfamily: 'nunito-bold',
         frontSize: 18,
+        color: '#333'
     }
 });
