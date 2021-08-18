097e2dfcac087d356acc493e371deca50767e58f
Angel_Brown <arbrown110@gmail.com>
2021-08-17 21:39:24 -0400

    importing navigator to app.js.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import * as Font from 'expo-font';
 import Home from './screens/home';
 import { AppLoading } from 'expo'
-
+import Navigator from './routes/homeStack'
 
 const getFonts = () => Font.loadAsync({
   
