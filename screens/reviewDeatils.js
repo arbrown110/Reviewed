22eaf0a3e8145b75d860132a85ab30659d062127
Angel_Brown <arbrown110@gmail.com>
2021-08-24 20:19:01 -0400

    using go back method to pop screen.

--- screens/reviewDeatils.js
+++ screens/reviewDeatils.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function RevieDetails() {
+export default function RevieDetails({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.goBack()
+    }
+
+
     return (
         <View> style={globalStyles.container}
             <Text>Detail Screen </Text>
-            <Button title '' onPress={pressHandler} />
+            <Button title ' back to home screen' onPress={pressHandler} />
         </View>
     )
 
 }
 
+
+
+// go back method pops screen off of stack
