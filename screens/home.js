b0bd7e4c33289aab1cdbde45122b7d0cf8ecc63a
Angel_Brown <arbrown110@gmail.com>
2021-09-01 19:07:57 -0400

    removing buttom.

--- screens/home.js
+++ screens/home.js
@@ -4,22 +4,17 @@ import { globalStyles } from '../styles/global';
 
 export default function Home({  navigaiton }) {
   
-  
-    const pressHandler = () => {
-      navigaiton.nagate('ReviewDetails');
-       // navigaiton.push('ReviewDetails');
 
-    }
     const [reviews, setReviews] = useState([
-        {title: 'Zelda, Breath of fresh Air', rating:5, body: 'lorem ipsum', key: '1'}
-        {title: 'Gotta catch Them All (again)', rating:4, body: 'lorem ipsum', key: '2'}
+        {title: 'Zelda, Breath of fresh Air', rating:5, body: 'lorem ipsum', key: '1'},
+        {title: 'Gotta catch Them All (again)', rating:4, body: 'lorem ipsum', key: '2'},
         {title: 'Not So "Final" Fantasy', rating:3, body: 'lorem ipsum', key: '3'}
     ]);
 
     return (
         <View> style={globalStyles.container}
             <Text style={globalStyles.titletext}>Home Screen </Text>
-            <Button title 'go to review details' onPress={pressHandler} />
+            {/* <Button title 'go to review details' onPress={pressHandler} /> */}
         </View>
     )
 
