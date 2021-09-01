627cf5d8e4a6d6160391970b07305c60082cdf52
Angel_Brown <arbrown110@gmail.com>
2021-09-01 19:16:57 -0400

    importing Flatlist and touchable opacity while removing button and text on stylesheet..

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home({  navigaiton }) {
+export default function Home({ navigaiton }) {
   
 
     const [reviews, setReviews] = useState([
-        {title: 'Zelda, Breath of fresh Air', rating:5, body: 'lorem ipsum', key: '1'},
-        {title: 'Gotta catch Them All (again)', rating:4, body: 'lorem ipsum', key: '2'},
-        {title: 'Not So "Final" Fantasy', rating:3, body: 'lorem ipsum', key: '3'}
+        {title: 'Zelda, Breath of fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+        {title: 'Gotta catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2'},
+        {title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
     ]);
 
     return (
         <View> style={globalStyles.container}
-            <Text style={globalStyles.titletext}>Home Screen </Text>
+            {/* <Text style={globalStyles.titletext}>Home Screen </Text> */}
             {/* <Button title 'go to review details' onPress={pressHandler} /> */}
         </View>
     )
