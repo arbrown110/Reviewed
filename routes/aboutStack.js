d42cca9ee7b5fff7aaf198a597d19f989f05ca05
Angel_Brown <arbrown110@gmail.com>
2021-09-22 20:13:48 -0400

    creating template from homestack for aboutstack.

---
+++ routes/aboutStack.js
@@ -0,0 +1,31 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigtion';
+import Home from '../screens/home';
+import ReviewDetails from '../screens/reviewDetails'
+
+
+const screens = {
+    Home: {
+        screen: Home,
+        navigationOptions:{
+            title: 'GameZone',
+            // headerStyle: { backgroundColor: '#eee'}
+        }
+    },
+    Reviewdetails: {
+        screen: ReviewDetails,
+        navigationOptions:{
+            title: 'Review Details',
+            // headerStyle: { backgroundColor: '#eee'}
+        }
+    }
+}
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#eee', height: 60 }
+
+    }
+});
+
+export default createAppContainer(HomeStack);
