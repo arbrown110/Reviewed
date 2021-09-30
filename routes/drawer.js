881bfacf46070c35c613a5c3e1f6bbba3464ed25
Angel_Brown <arbrown110@gmail.com>
2021-09-29 21:19:30 -0400

    adding About screen to drawer.

--- routes/drawer.js
+++ routes/drawer.js
@@ -3,6 +3,10 @@ import { createAppcontainer } from 'react-navigation';
 
 const RootDrawerNavigator = createDrawerNavigatior({
     home: {
-        screen: Homesick
+        screen: HomeStack,
+
+    },
+    About: {
+        
     }
 })
