f91c7bc9df9a87dabfd836095efc4f22ccc54c69
Angel_Brown <arbrown110@gmail.com>
2021-09-29 20:30:50 -0400

    setting up RootDrawerNavigator.

--- routes/drawer.js
+++ routes/drawer.js
@@ -1,2 +1,6 @@
 import { createDrawerNavigatior } from 'react-navigation-drawer';
 import { createAppcontainer } from 'react-navigation';
+
+const RootDrawerNavigator = createDrawerNavigatior({
+    
+})
