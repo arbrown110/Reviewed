c11b91cf323cacf1dfd7a436959b96c817aaf167
Angel_Brown <arbrown110@gmail.com>
2021-09-29 21:41:19 -0400

    correcting imports and imposting HomeStack.

--- routes/drawer.js
+++ routes/drawer.js
@@ -1,6 +1,7 @@
 import { createDrawerNavigatior } from 'react-navigation-drawer';
 import { createAppcontainer } from 'react-navigation';
-import About from '../screens/about';
+import AboutStack from '../aboutStack';
+import HomeStack from '../homestack';
 
 const RootDrawerNavigator = createDrawerNavigatior({
     home: {
