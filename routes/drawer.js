550252f319f8c7fbde501f9ea9a6da07d46afdcf
Angel_Brown <arbrown110@gmail.com>
2021-09-29 20:34:18 -0400

    adding screen to drawer.

--- routes/drawer.js
+++ routes/drawer.js
@@ -2,5 +2,7 @@ import { createDrawerNavigatior } from 'react-navigation-drawer';
 import { createAppcontainer } from 'react-navigation';
 
 const RootDrawerNavigator = createDrawerNavigatior({
-    
+    home: {
+        screen: Homesick
+    }
 })
