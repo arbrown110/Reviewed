59d1c67ea459fe5dd8305ff25af09bc0e678bcb4
Angel_Brown <arbrown110@gmail.com>
2021-10-07 19:07:27 -0400

    view for header text.

--- shared/header.js
+++ shared/header.js
@@ -4,5 +4,11 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 
 export default function Header() {
-    
+    return (
+        <View style={styles.header}>
+            <View>
+                <Text style={styles.headerText}></Text>
+            </View>
+        </View>
+    )
 }
