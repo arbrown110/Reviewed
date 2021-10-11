5a53ab5dd38248600f8c3e3dc38bce16aae9ffc9
Angel_Brown <arbrown110@gmail.com>
2021-10-11 19:02:35 -0400

    creating style sheet.

--- shared/header.js
+++ shared/header.js
@@ -6,9 +6,10 @@ import { MaterialIcons } from '@expo/vector-icons';
 export default function Header() {
     return (
         <View style={styles.header}>
+            {/*icon for the menu */}
             <View>
                 <Text style={styles.headerText}></Text>
             </View>
         </View>
-    )
+    );
 }
