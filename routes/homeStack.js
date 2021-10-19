b0b7ca0f2964f515dfe2ec291aac09dbc72d2bcc
Angel_Brown <arbrown110@gmail.com>
2021-10-19 19:40:15 -0400

    imports and headertitle

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -2,13 +2,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails'
-
+import Header from '../shared/header';
+import React from 'react';
 
 const screens = {
     Home: {
         screen: Home,
         navigationOptions:{
-            title: 'GameZone',
+            headerTitle: () => <Header />
   
         }
     },
